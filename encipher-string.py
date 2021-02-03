65bc8ecf5253c95bbc3d7f0e34ba8d63a51d6c04
Iryna Loza <irynloza@gmail.com>
2021-02-02 16:21:01 -0800

    Initial commit

---
+++ encipher-string.py
@@ -0,0 +1,30 @@
+"""Write a function that encrypts a string with a variable rotary cipher.
+
+The function should take in a number and string and shift the string's
+characters by that number:
+
+>>> rot_encode(1, 'abcxyz')
+'bcdyza'
+
+It should be able to shift characters by any number:
+
+>>> rot_encode(3, 'abcxyz')
+'defabc'
+
+It should preserve capitalization, whitespace, and any special characters:
+
+>>> rot_encode(1, 'wow! this is 100% amazing.')
+'xpx! uijt jt 100% bnbajoh.'
+"""
+
+
+def rot_encode(shift, txt):
+    """Encode `txt` by shifting its characters to the right."""
+
+   
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n✨ ALL TESTS PASSED!\n')
